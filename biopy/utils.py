a7214c71989454a2e05157df748983d6b69225b4
uuaxe <cweixin@gmail.com>
2026-03-03 11:07:17 +0800

    feat: implement subset and plot functions

--- biopy/utils.py
+++ biopy/utils.py
@@ -14,8 +14,7 @@ def subset(df, metadata, condition):
     return df[idx]
 
 
-def plot_umap(x, metadata, hue=None, alpha=1, figsize=(8,6), **kwargs):
-    fig, ax = plt.subplots(figsize=figsize)
+def plot_umap(ax, x, metadata, colourbar=False, **kwargs):
     reducer = umap.UMAP()
     z = reducer.fit_transform(x.transpose())
     z = pd.DataFrame(
@@ -29,17 +28,32 @@ def plot_umap(x, metadata, hue=None, alpha=1, figsize=(8,6), **kwargs):
         x='UMAP1',
         y='UMAP2',
         edgecolor=None,
-        hue=hue,
-        alpha=alpha,
         ax=ax,
         **kwargs
     )
+
+    if colourbar:
+        import matplotlib.dates as mdates
+        import matplotlib as mpl
+        hue = kwargs.pop('hue', None)
+        palette = kwargs.pop('palette', 'rocket')
+        # Create normalization for colourbar
+        norm = mpl.colors.Normalize(
+            vmin=metadata[hue].min(),
+            vmax=metadata[hue].max()
+        )
+        sm = mpl.cm.ScalarMappable(norm=norm, cmap=palette)
+        sm.set_array([])
+        fig = ax.get_figure()
+        cbar = fig.colorbar(sm, ax=ax)
+        cbar.set_label(hue)
+        cbar.ax.yaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
     return ax
 
 
-def plot_pca(x, metadata, hue=None, alpha=1, figsize=(8,6), **kwargs):
+def plot_pca(ax, x, metadata, colourbar=False, **kwargs):
     '''PCA plot for visualisation of batch effects.'''
-    fig, ax = plt.subplots(figsize=figsize)
     pca = PCA(n_components=2)
     z = pca.fit_transform(x.transpose())
     var_ratio = pca.explained_variance_ratio_
@@ -54,11 +68,27 @@ def plot_pca(x, metadata, hue=None, alpha=1, figsize=(8,6), **kwargs):
         x='PC1',
         y='PC2',
         edgecolor=None,
-        hue=hue,
-        alpha=alpha,
         ax=ax,
         **kwargs
     )
     ax.set_xlabel(f'PC1 ({var_ratio[0]*100:.1f}%)')
     ax.set_ylabel(f'PC2 ({var_ratio[1]*100:.1f}%)')
+
+    if colourbar:
+        import matplotlib.dates as mdates
+        import matplotlib as mpl
+        hue = kwargs.pop('hue', None)
+        palette = kwargs.pop('palette', 'rocket')
+        # Create normalization for colourbar
+        norm = mpl.colors.Normalize(
+            vmin=metadata[hue].min(),
+            vmax=metadata[hue].max()
+        )
+        sm = mpl.cm.ScalarMappable(norm=norm, cmap=palette)
+        sm.set_array([])
+        fig = ax.get_figure()
+        cbar = fig.colorbar(sm, ax=ax)
+        cbar.set_label(hue)
+        cbar.ax.yaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
     return ax
